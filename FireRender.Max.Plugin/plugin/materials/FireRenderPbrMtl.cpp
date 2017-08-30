b43af9af116ce01c6bbaefdf1755cb35ebe4b60a
DenisK <den13k@yandex.ru>
2017-08-30 12:02:41 +0300

    Implemented UI part.

--- FireRender.Max.Plugin/plugin/materials/FireRenderPbrMtl.cpp
+++ FireRender.Max.Plugin/plugin/materials/FireRenderPbrMtl.cpp
@@ -15,8 +15,21 @@ FIRERENDER_NAMESPACE_BEGIN;
 
 namespace
 {
-	static constexpr float RoughnessMin = 0.f;
-	static constexpr float RoughnessMax = 1.f;
+	static constexpr float RoughnessMulMin = 0.0f;
+	static constexpr float RoughnessMulMax = 1.0f;
+	static constexpr float RoughnessMulDefault = 0.0f;
+
+	static constexpr float MetalnessMulMin = 0.0f;
+	static constexpr float MetalnessMulMax = 1.0f;
+	static constexpr float MetalnessMulDefault = 0.0f;
+
+	static constexpr float CavityMulMin = 0.0f;
+	static constexpr float CavityMulMax = 1.0f;
+	static constexpr float CavityMulDefault = 0.0f;
+
+	static constexpr float NormalMulMin = 0.0f;
+	static constexpr float NormalMulMax = 1.0f;
+	static constexpr float NormalMulDefault = 0.0f;
 }
 
 IMPLEMENT_FRMTLCLASSDESC(PbrMtl)
@@ -30,22 +43,92 @@ static ParamBlockDesc2 pbDesc
     //rollout
 	IDD_FIRERENDER_PBRMTL, IDS_FR_MTL_PBR, 0, 0, NULL,
 
-	// DIFFUSE
+	// DIFFUSE COLOR
 	FRPBRMTL_DIFFUSE_COLOR_MUL, _T("DiffuseColorMul"), TYPE_FLOAT, P_ANIMATABLE, 0,
-	p_default, 1.0f, p_range, 0.f, FLT_MAX, p_ui, TYPE_SPINNER, EDITTYPE_FLOAT, IDC_PBR_DIFFUSE_COLOR_MUL, IDC_PBR_DIFFUSE_COLOR_MUL_S, SPIN_AUTOSCALE, PB_END,
+	p_default, 1.0f, p_range, 0.0f, FLT_MAX, p_ui, TYPE_SPINNER, EDITTYPE_FLOAT,
+	IDC_PBR_DIFFUSE_COLOR_MUL, IDC_PBR_DIFFUSE_COLOR_MUL_S, SPIN_AUTOSCALE, PB_END,
 
 	FRPBRMTL_DIFFUSE_COLOR, _T("DiffuseColor"), TYPE_RGBA, P_ANIMATABLE, 0,
-	p_default, Color(1.f, 1.f, 1.f), p_ui, TYPE_COLORSWATCH, IDC_PBR_DIFFUSE_COLOR, PB_END,
+	p_default, Color(1.0f, 1.0f, 1.0f), p_ui, TYPE_COLORSWATCH, IDC_PBR_DIFFUSE_COLOR, PB_END,
 
 	FRPBRMTL_DIFFUSE_COLOR_MAP, _T("DiffuseColorTexmap"), TYPE_TEXMAP, 0, 0,
 	p_subtexno, FRPBRMTL_MAP_DIFFUSE_COLOR, p_ui, TYPE_TEXMAPBUTTON, IDC_PBR_DIFFUSE_COLOR_MAP, PB_END,
 	
-    PB_END
+	FRPBRMTL_DIFFUSE_COLOR_USEMAP, _T("DiffuseColorUseMap"), TYPE_BOOL, P_ANIMATABLE, 0,
+	p_default, FALSE, p_ui, TYPE_SINGLECHEKBOX, IDC_PBR_DIFFUSE_COLOR_USEMAP, PB_END,
+
+	// ROUGHNESS
+	FRPBRMTL_ROUGHNESS_MUL, _T("RoughnessMul"), TYPE_FLOAT, P_ANIMATABLE, 0,
+	p_default, RoughnessMulDefault, p_range, RoughnessMulMin, RoughnessMulMax, p_ui, TYPE_SPINNER, EDITTYPE_FLOAT,
+	IDC_PBR_ROUGHNESS_MUL, IDC_PBR_ROUGHNESS_MUL_S, SPIN_AUTOSCALE, PB_END,
+
+	FRPBRMTL_ROUGHNESS, _T("Roughness"), TYPE_RGBA, P_ANIMATABLE, 0,
+	p_default, Color(1.0f, 1.0f, 1.0f), p_ui, TYPE_COLORSWATCH, IDC_PBR_ROUGHNESS, PB_END,
+
+	FRPBRMTL_ROUGHNESS_MAP, _T("RoughnessTexmap"), TYPE_TEXMAP, 0, 0,
+	p_subtexno, FRPBRMTL_MAP_ROUGHNESS, p_ui, TYPE_TEXMAPBUTTON, IDC_PBR_ROUGHNESS_MAP, PB_END,
+	
+	FRPBRMTL_ROUGHNESS_USEMAP, _T("RoughnessUseMap"), TYPE_BOOL, P_ANIMATABLE, 0,
+	p_default, FALSE, p_ui, TYPE_SINGLECHEKBOX, IDC_PBR_ROUGHNESS_USEMAP, PB_END,
+
+	FRPBRMTL_ROUGHNESS_INVERT, _T("RoughnessInvert"), TYPE_BOOL, P_ANIMATABLE, 0,
+	p_default, FALSE, p_ui, TYPE_SINGLECHEKBOX, IDC_PBR_ROUGHNESS_INVERT, PB_END,
+
+	// METALNESS
+	FRPBRMTL_METALNESS_MUL, _T("MetalnessMul"), TYPE_FLOAT, P_ANIMATABLE, 0,
+	p_default, MetalnessMulDefault, p_range, MetalnessMulMin, MetalnessMulMax, p_ui, TYPE_SPINNER, EDITTYPE_FLOAT,
+	IDC_PBR_METALNESS_MUL, IDC_PBR_METALNESS_MUL_S, SPIN_AUTOSCALE, PB_END,
+
+	FRPBRMTL_METALNESS, _T("Metalness"), TYPE_RGBA, P_ANIMATABLE, 0,
+	p_default, Color(1.0f, 1.0f, 1.0f), p_ui, TYPE_COLORSWATCH, IDC_PBR_METALNESS, PB_END,
+
+	FRPBRMTL_METALNESS_MAP, _T("MetalnessTexmap"), TYPE_TEXMAP, 0, 0,
+	p_subtexno, FRPBRMTL_MAP_METALNESS, p_ui, TYPE_TEXMAPBUTTON, IDC_PBR_METALNESS_MAP, PB_END,
+
+	FRPBRMTL_METALNESS_USEMAP, _T("MetalnessUseMap"), TYPE_BOOL, P_ANIMATABLE, 0,
+	p_default, FALSE, p_ui, TYPE_SINGLECHEKBOX, IDC_PBR_METALNESS_USEMAP, PB_END,
+
+	// CAVITY
+	FRPBRMTL_CAVITY_MUL, _T("CavityMul"), TYPE_FLOAT, P_ANIMATABLE, 0,
+	p_default, CavityMulDefault, p_range, CavityMulMin, CavityMulMax, p_ui, TYPE_SPINNER, EDITTYPE_FLOAT,
+	IDC_PBR_CAVITY_MUL, IDC_PBR_CAVITY_MUL_S, SPIN_AUTOSCALE, PB_END,
+
+	FRPBRMTL_CAVITY, _T("Cavity"), TYPE_RGBA, P_ANIMATABLE, 0,
+	p_default, Color(1.f, 1.f, 1.f), p_ui, TYPE_COLORSWATCH, IDC_PBR_CAVITY, PB_END,
+
+	FRPBRMTL_CAVITY_MAP, _T("CavityTexmap"), TYPE_TEXMAP, 0, 0,
+	p_subtexno, FRPBRMTL_MAP_CAVITY, p_ui, TYPE_TEXMAPBUTTON, IDC_PBR_CAVITY_MAP, PB_END,
+
+	FRPBRMTL_CAVITY_USEMAP, _T("CavityUseMap"), TYPE_BOOL, P_ANIMATABLE, 0,
+	p_default, FALSE, p_ui, TYPE_SINGLECHEKBOX, IDC_PBR_CAVITY_USEMAP, PB_END,
+
+	FRPBRMTL_CAVITY_INVERT, _T("CavityInvert"), TYPE_BOOL, P_ANIMATABLE, 0,
+	p_default, FALSE, p_ui, TYPE_SINGLECHEKBOX, IDC_PBR_CAVITY_INVERT, PB_END,
+
+	// NORMAL
+	FRPBRMTL_NORMAL_MUL, _T("NormalMul"), TYPE_FLOAT, P_ANIMATABLE, 0,
+	p_default, NormalMulDefault, p_range, NormalMulMin, NormalMulMax, p_ui, TYPE_SPINNER, EDITTYPE_FLOAT,
+	IDC_PBR_NORMAL_MUL, IDC_PBR_NORMAL_MUL_S, SPIN_AUTOSCALE, PB_END,
+
+	FRPBRMTL_NORMAL, _T("Normal"), TYPE_RGBA, P_ANIMATABLE, 0,
+	p_default, Color(1.0f, 1.0f, 1.0f), p_ui, TYPE_COLORSWATCH, IDC_PBR_NORMAL, PB_END,
+
+	FRPBRMTL_NORMAL_MAP, _T("NormalTexmap"), TYPE_TEXMAP, 0, 0,
+	p_subtexno, FRPBRMTL_MAP_NORMAL, p_ui, TYPE_TEXMAPBUTTON, IDC_PBR_NORMAL_MAP, PB_END,
+
+	FRPBRMTL_NORMAL_USEMAP, _T("NormalUseMap"), TYPE_BOOL, P_ANIMATABLE, 0,
+	p_default, FALSE, p_ui, TYPE_SINGLECHEKBOX, IDC_PBR_NORMAL_USEMAP, PB_END,
+
+	PB_END
 );
 
 std::map<int, std::pair<ParamID, MCHAR*>> FRMTLCLASSNAME(PbrMtl)::TEXMAP_MAPPING =
 {
 	{ FRPBRMTL_MAP_DIFFUSE_COLOR, { FRPBRMTL_DIFFUSE_COLOR_MAP, _T("Diffuse Color") } },
+	{ FRPBRMTL_MAP_ROUGHNESS,     { FRPBRMTL_ROUGHNESS_MAP,     _T("Roughness") } },
+	{ FRPBRMTL_MAP_METALNESS,     { FRPBRMTL_METALNESS_MAP,     _T("Metalness") } },
+	{ FRPBRMTL_MAP_CAVITY,        { FRPBRMTL_CAVITY_MAP,        _T("Cavity Map") } },
+	{ FRPBRMTL_MAP_NORMAL,        { FRPBRMTL_NORMAL_MAP,        _T("Normal Map") } },
 };
 
 FRMTLCLASSNAME(PbrMtl)::~FRMTLCLASSNAME(PbrMtl)()
